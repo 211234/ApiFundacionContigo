8a7fe3079595cb7d565f056e01fbbf44e7d0bba3
josueCastillejos <84791173+211234@users.noreply.github.com>
2024-12-07 23:34:28 -0600

    Implement API gateway with rate limiting and environment configuration

---
+++ gateway/src/config/env.ts
@@ -0,0 +1,46 @@
+import { config } from "dotenv";
+import Joi from "joi";
+
+config();
+
+const envSchema = Joi.object({
+  PORT: Joi.number().required(),
+  USERS_SERVICE_URL: Joi.string().uri().required(),
+  NOTIFICATIONS_SERVICE_URL: Joi.string().uri().required(),
+  CASES_SERVICE_URL: Joi.string().uri().required(),
+}).unknown();
+
+const { error, value: envVars } = envSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const {
+  PORT,
+  USERS_SERVICE_URL,
+  NOTIFICATIONS_SERVICE_URL,
+  CASES_SERVICE_URL,
+} = envVars;
+
+interface Env {
+  port: {
+    PORT: number;
+  };
+  services: {
+    USERS_SERVICE_URL: string;
+    NOTIFICATIONS_SERVICE_URL: string;
+    CASES_SERVICE_URL: string;
+  };
+}
+
+export const env: Env = {
+  port: {
+    PORT: PORT,
+  },
+  services: {
+    USERS_SERVICE_URL: USERS_SERVICE_URL,
+    NOTIFICATIONS_SERVICE_URL: NOTIFICATIONS_SERVICE_URL,
+    CASES_SERVICE_URL: CASES_SERVICE_URL,
+  },
+};
